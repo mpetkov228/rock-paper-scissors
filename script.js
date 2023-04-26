4a5bc47f90801556fb6800aec8f0a192239e0af4
mpetkov228 <mpetkov228@gmail.com>
2023-04-26 15:03:25 +0300

    Add playRound function

--- script.js
+++ script.js
@@ -18,3 +18,40 @@ function getPlayerChoice() {
 
     return playerChoice;
 }
+
+let computerChoice = getComputerChoice();
+let playerChoice = getPlayerChoice();
+
+function playRound(computerChoice, playerChoice) {
+    switch (computerChoice) {
+        case 'rock':
+            if (playerChoice == 'rock') {
+                return 'It\'s a tie!';
+            } else if (playerChoice == 'paper') {
+                return 'You Win! Paper beats Rock!';
+            } else if (playerChoice == 'scissors') {
+                return 'You Lose! Rock beats Scissors!';
+            }
+        break;
+        case 'paper':
+            if (playerChoice == 'rock') {
+                return 'You Lose! Paper beats Rock!';
+            } else if (playerChoice == 'paper') {
+                return 'It\'s a tie!';
+            } else if (playerChoice == 'scissors') {
+                return 'You Win! Scissors beat Paper!';
+            }
+        break;
+        case 'scissors':
+            if (playerChoice == 'rock') {
+                return 'You Win! Rock beats Scissors!';
+            } else if (playerChoice == 'paper') {
+                return 'You Lose! Scissors beat Paper!';
+            } else if (playerChoice == 'scissors') {
+                return 'It\'s a tie!';
+            }
+        break;
+    }
+}
+
+playRound(computerChoice, playerChoice);
