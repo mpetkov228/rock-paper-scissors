bd307bf6c241be2ed30acd784821364a96f815b7
mpetkov228 <mpetkov228@gmail.com>
2023-04-26 14:42:17 +0300

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('added script!');
+const choices = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+    // Function generates a random number between 0 and 2 
+    // which is used to index into choices array
+    // returns random item from array
+
+    let index = Math.floor(Math.random() * 3);
+
+    return choices[index];
+}
